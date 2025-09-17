08979033151010dd9d6690f6a34118d343019964
Gillian <gillian.delvigne@gmail.com>
2025-09-17 22:05:20 +0200

    auto commit

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const App = () => {
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState("");
+
+    const handlePerson = (event) => {
+        event.preventDefault();
+        const contactList = [...persons, { name: newName }];
+        setPersons(contactList);
+		setNewName("");
+    };
+
+    const handleName = (event) => {
+        setNewName(event.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handlePerson}>
+                <div>
+                    name: <input value={newName} onChange={handleName} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+           {persons.map(person => <p key={person.name}>{person.name}</p> )}
+        </div>
+    );
+};
+
+export default App;
