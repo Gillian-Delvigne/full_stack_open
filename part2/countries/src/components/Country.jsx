70dbbdf5f2e4d573426a27e68bc569dfa9a1f8f4
Gillian <gillian.delvigne@gmail.com>
2025-09-19 16:02:19 +0200

    auto commit

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -6,7 +6,7 @@ export const Country = ({ countries }) => {
             {countries.length === 1 && (
                 <div>
                     <h1>{countries[0].name.common}</h1>
-                    <p>Capital {countries[0].capital}</p>
+                    <p>Capital {countries[0].capital[0]}</p>
                     <p>Area {countries[0].area}</p>
                     <h2>Languages</h2>
                     <ul>
