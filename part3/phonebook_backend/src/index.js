d2b142a7ee2097e2c678d98e08d43c76bbe942a8
Gillian <gillian.delvigne@gmail.com>
2025-09-22 13:03:07 +0200

    auto commit

--- part3/phonebook_backend/src/index.js
+++ part3/phonebook_backend/src/index.js
@@ -1,12 +1,63 @@
 const express = require("express");
 
-const persons = require("./data");
+const data = require("./data");
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/info", (request, response) => {
+    const timestamp = Date();
+    const info = `<p>Phonebook has info for ${data.persons.length} people</p><p>${timestamp}</p>`;
+    response.send(info);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+    const person = data.persons.find(
+        (persons) => persons.id === request.params.id
+    );
+    if (person) response.json(person);
+    else response.status(404).end();
+});
+
 app.get("/api/persons", (request, response) => {
-	response.json(persons);
-})
+    if (data.persons) response.json(data.persons);
+    else response.status(404).end();
+});
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body;
+
+    if (!body) {
+        return response.status(400).json({
+            error: "person missing",
+        });
+    }
+    if (!body.name || !body.number)
+        return response.status(400).json({
+            error: "name or number missing",
+        });
+
+    if (data.persons.find((person) => person.name === body.name))
+        return response.status(400).json({
+            error: "name must be unique",
+        });
+    const id = (Math.random() * 10000).toString();
+    const person = { id: id, ...body };
+    data.persons = data.persons.concat(person);
+    response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+    const person = data.persons.find(
+        (person) => person.id === request.params.id
+    );
+    if (person) {
+        data.persons = data.persons.filter((p) => p.id !== person.id);
+        console.log(data.persons);
+        response.status(204).end();
+    } else response.status(404).end();
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
